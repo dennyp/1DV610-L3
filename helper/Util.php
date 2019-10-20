10256d795b3a02c1ee0e4e7ab1dc728e2c05dbf8
Denny Petersson <petersson.denny@gmail.com>
2019-10-20 23:09:03 +0200

    adding type safety

--- helper/Util.php
+++ helper/Util.php
@@ -19,17 +19,17 @@ class Util
         return !is_null($str) && $str !== '';
     }
 
-    public static function isNotMatch($left, $right)
+    public static function isNotMatch(string $left, string $right): bool
     {
         return $left !== $right;
     }
 
-    public static function isNotNull($str)
+    public static function isNotNull(string $str): bool
     {
         return !is_null($str);
     }
 
-    public static function removeBadCharacters($rawString)
+    public static function removeBadCharacters(string $rawString): string
     {
         return filter_var($rawString, FILTER_SANITIZE_STRING);
     }
